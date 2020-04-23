1f1c95f0cf99e115242d0f4177096220acf8d322
cyproto <kyashkarande@gmail.com>
2020-04-23 19:32:44 +0530

    shared: Add auth guard and auth service.

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
-@Injectable()
+@Injectable({providedIn: "root"})
 export class AuthGuard implements CanActivate {
-    constructor(private router: Router) {}
+    constructor(private router: Router, private authService: AuthService) {}
 
     canActivate() {
-        if (localStorage.getItem('isLoggedin')) {
-            return true;
+        console.log(this.authService.isAuthenticated());
+        if ( !this.authService.isAuthenticated() ) {
+            this.router.navigate(['/login']);
+            return false;
         }
-
-        this.router.navigate(['/login']);
-        return false;
+        return true;
     }
 }
