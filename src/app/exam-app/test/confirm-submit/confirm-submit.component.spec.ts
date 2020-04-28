4d7b0a37478a2c9cd8c12b873304e417cfb14efd
cyproto <kyashkarande@gmail.com>
2020-04-29 01:42:35 +0530

    confirm-submit: Initial commit.

---
+++ src/app/exam-app/test/confirm-submit/confirm-submit.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConfirmSubmitComponent } from './confirm-submit.component';
+
+describe('ConfirmSubmitComponent', () => {
+  let component: ConfirmSubmitComponent;
+  let fixture: ComponentFixture<ConfirmSubmitComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ConfirmSubmitComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ConfirmSubmitComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/exam-app/test/confirm-submit/confirm-submit.component.ts
@@ -0,0 +1,24 @@
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Component, OnInit, Inject } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-submit',
+  templateUrl: './confirm-submit.component.html',
+  styleUrls: ['./confirm-submit.component.scss']
+})
+export class ConfirmSubmitComponent implements OnInit {
+
+  constructor(public dialogRef: MatDialogRef<ConfirmSubmitComponent>,
+    @Inject(MAT_DIALOG_DATA) public data) {
+  }
+
+  ngOnInit() {
+  }
+  onConfirm(): void {
+    this.dialogRef.close(true);
+  }
+
+  onDismiss(): void {
+    this.dialogRef.close(false);
+  }
+}
